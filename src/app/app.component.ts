ee07cbb119ac768fe52324864cd6dc88f3f70128
Bruno Da Silva Pinto <brunodspinto98@gmail.com>
2024-09-24 11:13:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListeTransactionsComponent } from './liste-transactions/liste-transactions.component';
+import { CommonModule } from '@angular/common';
+import { HeureComponent } from './heure/heure.component';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    ListeTransactionsComponent,
+    HeureComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exercice3';
+
+  currentTime: Date = new Date();
+
+  ngOnInit() {
+    interval(1000).pipe( 
+      map(() => new Date()) 
+    ).subscribe(time => {
+      this.currentTime = time;
+    });
+  }
 }
